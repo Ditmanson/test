d3c1b3a66ba80a86961e51d48cde7cb4525c68a1
Travis Ditmanson <tdtimans@uccs.edu>
2024-01-06 20:35:37 -0700

    andSoTheTest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,68 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'my-app';
+  length = 0;
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onButtonClick() {
+    console.log(`
+      About to generate password that includes:
+      Includes Letters: ${this.includeLetters}
+      Includes Numbers: ${this.includeNumbers}
+      Includes Symbols: ${this.includeSymbols}
+      `)
+      this.password='password';
+  }
+
+  generatePassword() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onChangeLength(target: EventTarget) {
+    const value = (<HTMLInputElement>target).value;
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+    console.log('Use Numbers toggled');
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+    console.log('Use Symbols toggled');
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+    console.log('Use Letters toggled');
+  }
+
+
 }
